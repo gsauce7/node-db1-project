67dfdd48f7a9c5540f56cc0b72f3cda72314cfb3
Gabriel Henton <gabehenton@gmail.com>
2021-06-16 21:53:08 -0700

    accounts model complete

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,48 @@
+const db = require("../../data/db-config")
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db("accounts") // select * from accounts
+
 }
+
 
 const getById = id => {
   // DO YOUR MAGIC
+  // select * from accounts where id = userProvidedId
+  return db("accounts").where("id", id).first()
+  // return db("accounts").where("id", id)
+
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+
+// const create = account => {
+//   // DO YOUR MAGIC
+// }
+async function create({ name, budget }) {
+  const [id] = await db("accounts").insert({ name, budget })
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+
+
+// const updateById = (id, account) => {
+//   // DO YOUR MAGIC
+// }
+
+async function updateById(id, { name, budget }) {
+  await db("accounts").where("id", id).update({ name, budget })
+  return getById(id)
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+// const deleteById = id => {
+//   // DO YOUR MAGIC
+// }
+
+async function deleteById(id) {
+  const deletedAccount = await getById(id)
+  await db("accounts").where("id", id).delete()
+  return deletedAccount
 }
 
 module.exports = {
